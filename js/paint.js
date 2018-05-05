39fe08c65c92394b38ab0bbf4ce7bebb4fdbadb7
SanicRekt <38928604+SanicRekt@users.noreply.github.com>
2018-05-05 17:37:07 -0300

    Update paint.js

--- js/paint.js
+++ js/paint.js
@@ -1,176 +1,65 @@
-var arr_touches = [];
-var canvas;
-var ctx;
-var down = false; 
-var color = 'black';
-var width = 5; 
+var canvas = document.getElementById ('canvas');
+var context = canvas.getContext('2d');
+context.lineWidth = 5;
+var down = false;
 
+canvas.addEventListener('mousemove', draw);
 
+canvas.addEventListener('mousedown',function()
+{
+	down = true;
+	context.beginPath();
+	context.moveTo(xPos, yPos);
+	canvas.addEventListener('mousemove',draw);
+});
 
-window.onload = function() {
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d'); 
-    ctx.lineWidth = width;
-    
+canvas.addEventListener('mouseup',function(){ down = false; });
 
-    canvas.addEventListener('mousemove', handleMove); 
-    canvas.addEventListener('mousedown', handleDown);
-    canvas.addEventListener('mouseup', handleUp);
-    
-    //handling mobile touch events
-    canvas.addEventListener("touchstart", handleStart, false);
-    canvas.addEventListener("touchend", handleEnd, false);
-    canvas.addEventListener("touchcancel", handleCancel, false);
-    canvas.addEventListener("touchleave", handleEnd, false);
-    canvas.addEventListener("touchmove", handleTouchMove, false);
-};
-function handleMove(e)
+function draw (e)
 {
-    xPos = e.clientX-canvas.offsetLeft;
-    yPos = e.clientY-canvas.offsetTop;
-    if(down == true)
-    {
-        ctx.lineTo(xPos,yPos); 
-        ctx.strokeStyle = color;
-        ctx.stroke();
-    }
-}
-function handleDown() 
-{
-    down = true;
-    ctx.beginPath();
-    ctx.moveTo(xPos, yPos);
-}
-function handleUp() 
-{
-    down = false;
-}
-function handleStart(evt) 
-{
-    var touches = evt.changedTouches;
-    for(var i = 0; i < touches.length; i++) 
-    {
-        if(isValidTouch(touches[i])) 
-        {
-            evt.preventDefault();
-            arr_touches.push(copyTouch(touches[i]));
-            ctx.beginPath();
-            ctx.fillStyle = color;
-            ctx.fill();
-        }
-    }
-}
-function handleTouchMove(evt) 
-{
-    var touches = evt.changedTouches;
-    var offset = findPos(canvas);
-    for (var i = 0; i < touches.length; i++) 
-    {
-        if(isValidTouch(touches[i])) 
-        {
-            evt.preventDefault();
-            var idx = ongoingTouchIndexById(touches[i].identifier);
-            if (idx >= 0) 
-            {
-                ctx.beginPath();
-                ctx.moveTo(arr_touches[idx].clientX-offset.x, arr_touches[idx].clientY-offset.y);
-                ctx.lineTo(touches[i].clientX-offset.x, touches[i].clientY-offset.y);
-                ctx.strokeStyle = color;
-                ctx.stroke();
-                
-                arr_touches.splice(idx, 1, copyTouch(touches[i]));
-            }   
-        }
-    }
-}
-function handleEnd(evt) 
-{
-    var touches = evt.changedTouches;
-    var offset = findPos(canvas);
-    for (var i = 0; i < touches.length; i++) 
-    {
-        if(isValidTouch(touches[i])) 
-        {
-            evt.preventDefault();
-            var idx = ongoingTouchIndexById(touches[i].identifier);
-            if (idx >= 0) 
-            {
-                ctx.lineWidth = 4;
-                ctx.fillStyle = color;
-                ctx.beginPath();
-                ctx.moveTo(arr_touches[idx].clientX-offset.x, arr_touches[idx].clientY-offset.y);
-                ctx.lineTo(touches[i].clientX-offset.x, touches[i].clientY-offset.y);
-               arr_touches.splice(i, 1);
-            } 
-        }
-    }
-}
-function handleCancel(evt) 
-{
-    evt.preventDefault();
-    var touches = evt.changedTouches;
-  
-    for (var i = 0; i < touches.length; i++) {
-        arr_touches.splice(i, 1);
-    }
-}
-function copyTouch(touch) 
-{
-    return {identifier: touch.identifier,clientX: touch.clientX,clientY: touch.clientY};
-}
-function ongoingTouchIndexById(idToFind) 
-{
-    for (var i = 0; i < arr_touches.length; i++) {
-        var id = arr_touches[i].identifier;
-        if (id == idToFind) {
-            return i;
-        }
-    }
-    return -1;
-}
-function changeColor(new_color) 
-{
-    color = new_color;
-}
-function clearCanvas() 
-{
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-function isValidTouch(touch) 
-{
-    var curleft = 0, curtop = 0;
-    var offset = 0;
-    
-    if (canvas.offsetParent) {
-        do {
-            curleft += canvas.offsetLeft;
-            curtop += canvas.offsetTop;
-        } while (touch == canvas.offsetParent);
-        
-        offset = { x: curleft-document.body.scrollLeft, y: curtop-document.body.scrollTop };
-    }
-    
-    if(touch.clientX-offset.x > 0 &&
-            touch.clientX-offset.x < parseFloat(canvas.width) &&
-                touch.clientY-offset.y >0 &&
-                    touch.clientY-offset.y < parseFloat(canvas.height)) {
-        return true;
-    }
-    else 
-    {
-        return false;
-    }
-}
-function findPos(obj) 
-{
-    var curleft = 0, curtop = 0;
-    if (obj.offsetParent) 
-    {
-        do {
-            curleft += obj.offsetLeft;
-            curtop += obj.offsetTop;
-        } while (obj == obj.offsetParent);
+	xPos = e.clientX - canvas.offsetLeft;
+	yPos = e.clientY - canvas.offsetTop;
 
-        return { x: curleft-document.body.scrollLeft, y: curtop-document.body.scrollTop };
-    }
+	if (down == true)
+	{
+		context.lineTo(xPos, yPos);
+		context.stroke();
+	}
 }
+
+function changeColor(color) {context.strokeStyle = color; context.fillStyle = color;}
+function clearCanvas() { context.clearRect(0, 0, canvas.width, canvas.height); }
+function changeBrushSize(size) {context.lineWidth = size;}
+function fillCanvas () { context.fillRect(0, 0, canvas.width, canvas.height);}
+function triggerClick() {document.getElementById('file').click();}
+
+document.getElementById('file').addEventListener('change', function(e)
+{
+	clearCanvas();
+	URL = URL || webkitURL;
+	var temp= URL.createObjectURL(e.target.files[0]);
+	var image= new Image ();
+	image.src= temp;
+
+	image.addEventListener('load',function()
+	{
+		imageWidth=image.naturalWidth;
+		imageHeight=image.naturalHeight;
+		newImageWidth=imageWidth;
+		newImageHeight=imageHeight;
+		originalImageRatio=imageWidth / imageHeight;
+
+		if(newImageWidth > newImageHeight && newImageWidth > 800)
+		{
+			newImageWidth=800;
+			newImageHeight=800 / originalImageRatio;
+		}
+		if((newImageWidth >= newImageHeight || newImageHeight > newImageWidth) && newImageHeight > 500)
+		{
+			newImageHeight=500;
+			newImageWidth= 500 * originalImageRatio;
+		}
+		context.drawImage(image, 0, 0, newImageWidth, newImageHeight);
+		URL.revokeObjectURL(temp);
+	});
+});
