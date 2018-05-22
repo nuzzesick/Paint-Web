6d46bef5b951f9db52ee569cd24efd70672ae2a3
Nuzze <mati280341@gmail.com>
2018-05-22 18:10:46 -0300

    Update paint.js

--- js/paint.js
+++ js/paint.js
@@ -7,24 +7,30 @@ canvas.addEventListener('mousemove', draw);
 
 canvas.addEventListener('mousedown',function()
 {
-	down = true;
-	context.beginPath();
-	context.moveTo(xPos, yPos);
-	canvas.addEventListener('mousemove',draw);
+    down = true;
+    context.beginPath();
+    context.moveTo(xPos, yPos);
+    canvas.addEventListener('mousemove',draw);
 });
 
 canvas.addEventListener('mouseup',function(){ down = false; });
+window.onload = function round() {
+var c=document.getElementById('canvas');
+var ctx=c.getContext('2d');
+ctx.lineCap='round';
+ }
+
 
 function draw (e)
 {
-	xPos = e.clientX - canvas.offsetLeft;
-	yPos = e.clientY - canvas.offsetTop;
+    xPos = e.clientX - canvas.offsetLeft;
+    yPos = e.clientY - canvas.offsetTop;
 
-	if (down == true)
-	{
-		context.lineTo(xPos, yPos);
-		context.stroke();
-	}
+    if (down == true)
+    {
+        context.lineTo(xPos, yPos);
+        context.stroke();
+    }
 }
 
 function changeColor(color) {context.strokeStyle = color; context.fillStyle = color;}
@@ -32,34 +38,35 @@ function clearCanvas() { context.clearRect(0, 0, canvas.width, canvas.height); }
 function changeBrushSize(size) {context.lineWidth = size;}
 function fillCanvas () { context.fillRect(0, 0, canvas.width, canvas.height);}
 function triggerClick() {document.getElementById('file').click();}
+function changeBrushStyle(brushStyle) {context.lineCap=brushStyle;}
 
 document.getElementById('file').addEventListener('change', function(e)
 {
-	clearCanvas();
-	URL = URL || webkitURL;
-	var temp= URL.createObjectURL(e.target.files[0]);
-	var image= new Image ();
-	image.src= temp;
+    clearCanvas();
+    URL = URL || webkitURL;
+    var temp= URL.createObjectURL(e.target.files[0]);
+    var image= new Image ();
+    image.src= temp;
 
-	image.addEventListener('load',function()
-	{
-		imageWidth=image.naturalWidth;
-		imageHeight=image.naturalHeight;
-		newImageWidth=imageWidth;
-		newImageHeight=imageHeight;
-		originalImageRatio=imageWidth / imageHeight;
+    image.addEventListener('load',function()
+    {
+        imageWidth=image.naturalWidth;
+        imageHeight=image.naturalHeight;
+        newImageWidth=imageWidth;
+        newImageHeight=imageHeight;
+        originalImageRatio=imageWidth / imageHeight;
 
-		if(newImageWidth > newImageHeight && newImageWidth > 800)
-		{
-			newImageWidth=800;
-			newImageHeight=800 / originalImageRatio;
-		}
-		if((newImageWidth >= newImageHeight || newImageHeight > newImageWidth) && newImageHeight > 500)
-		{
-			newImageHeight=500;
-			newImageWidth= 500 * originalImageRatio;
-		}
-		context.drawImage(image, 0, 0, newImageWidth, newImageHeight);
-		URL.revokeObjectURL(temp);
-	});
+        if(newImageWidth > newImageHeight && newImageWidth > 800)
+        {
+            newImageWidth=800;
+            newImageHeight=800 / originalImageRatio;
+        }
+        if((newImageWidth >= newImageHeight || newImageHeight > newImageWidth) && newImageHeight > 500)
+        {
+            newImageHeight=500;
+            newImageWidth= 500 * originalImageRatio;
+        }
+        context.drawImage(image, 0, 0, newImageWidth, newImageHeight);
+        URL.revokeObjectURL(temp);
+    });
 });
